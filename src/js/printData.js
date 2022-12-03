09885075c8dc2d461a0d3c9391f9c2e72467d2b1
JSamuel <sp4619168@gmail.com>
2022-12-03 13:01:36 -0600

    docs: Documentar la funcion printTeams

--- src/js/printData.js
+++ src/js/printData.js
@@ -1,5 +1,9 @@
 import Table from "./main.js";
 
+/**
+ * Set the state of the Table component and render it
+ * @param teams - an array of objects, each object representing a team.
+ */
 const printTeams = (teams) => {
 	Table.setState({ teamsList: teams });
 };
