b27023f716f187791e22f117f0c1717702b61a89
JSamuel <sp4619168@gmail.com>
2022-12-02 22:10:58 -0600

    feat: Clase para crear componentes creada

---
+++ src/js/Component.js
@@ -0,0 +1,65 @@
+/**
+ * Create a component
+ * @param {function} options
+ * @class
+ * @classdesc Class to create a reactive component.
+ * @returns {Object}
+ */
+const Component = (function () {
+	/**
+	 * @constructs
+	 * @param {Object} options
+	 * @type {String} el Element ID
+	 * @type {Object} data State
+	 * @type {function} template Function to create the inner HTML element
+	 * @example
+	 *  new Component({
+	 *    el: "#button",
+	 *    data: {value: 'Show'},
+	 *    template: function (props) {
+	 *      return `<span>${props.value}</span>`
+	 *    }
+	 *  })
+	 */
+	const Constructor = function (options) {
+		this.el = options.el;
+		this.data = options.data;
+		this.template = options.template;
+	};
+
+	/**
+	 * Embed the HTML template inside the element
+	 * @method
+	 */
+	Constructor.prototype.render = function () {
+		const $el = document.querySelector(this.el);
+
+		if (!$el) return;
+
+		$el.innerHTML = this.template(this.data);
+	};
+
+	/**
+	 * Update the State and render
+	 * @method
+	 * @param {Object} obj New values of the State
+	 */
+	Constructor.prototype.setState = function (obj) {
+		for (let key in obj) {
+			if (this.data.hasOwnProperty(key)) {
+				this.data[key] = obj[key];
+			}
+			this.render();
+		}
+	};
+
+	/**
+	 * Get the State
+	 * @method
+	 */
+	Constructor.prototype.getState = function () {
+		return JSON.parse(JSON.stringify(this.data));
+	};
+
+	return Constructor;
+})();
